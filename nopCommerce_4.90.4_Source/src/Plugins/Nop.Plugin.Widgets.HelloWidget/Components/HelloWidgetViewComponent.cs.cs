0fd3e94f8433ab9351a5d2090737357dd12b0da4
shahadkhraisheh96 <shahadkh2004@gmail.com>
2026-05-29 14:10:17 +0300

    add

---
+++ nopCommerce_4.90.4_Source/src/Plugins/Nop.Plugin.Widgets.HelloWidget/Components/HelloWidgetViewComponent.cs.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.HelloWidget.Components
+{
+    public class HelloWidgetViewComponent : NopViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+        {
+            // This maps exactly to the view folder structure we will create next
+            return View("~/Plugins/Widgets.HelloWidget/Views/PublicInfo.cshtml");
+        }
+    }
+}
---
+++ nopCommerce_4.90.4_Source/src/Plugins/Nop.Plugin.Widgets.HelloWidget/HelloWidgetPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nop.Services.Cms;
+using Nop.Plugin.Widgets.HelloWidget.Components;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.HelloWidget
+{
+    public class HelloWidgetPlugin : BasePlugin, IWidgetPlugin
+    {
+        public bool HideInWidgetList => false;
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            // Directs the widget to load on the top of the store homepage
+            return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.HomepageTop });
+        }
+
+        public Type GetWidgetViewComponent(string widgetZone)
+        {
+            // We will build this view component next
+            return typeof(Components.HelloWidgetViewComponent);
+        }
+
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+    }
+}
